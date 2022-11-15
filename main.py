11726726483e60d08889f4c72708e81e9281d18c
machine-science <machinescientist@outlook.com>
2022-11-16 00:19:49 +0530

    Added buttons to the frames

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, Tk, Frame, Button
+from tkinter import ttk, Tk, Frame, Button, BOTTOM, CENTER
 
 
 class Main(object):
@@ -6,16 +6,29 @@ class Main(object):
         self.master = master
         
         mainFrame = Frame(self.master, relief="sunken", borderwidth=4)
-        mainFrame.pack()
+        mainFrame.place()
         
-        middelFrame = Frame(mainFrame, width=1350, bg='red', relief='sunken')
+          
+        # middelFrame1 = Frame(mainFrame, width=400, bg='red', relief='sunken')
+        # middelFrame1.pack(side=BOTTOM)
+        
+        # Center frame
+        centerFrame = Frame(self.master, width=400, height=200, bg='#435EA9', relief='sunken')
+        centerFrame.place(relx=.5, rely=.5,anchor= CENTER)
+        
+        #Add buttons
+        addButton = Button(centerFrame, width=50, height=2, bg='#63CD41', relief='sunken')
+        addButton.place(relx=.5, rely=.2,anchor= CENTER)
+        
+        addButton = Button(centerFrame, width=50, height=2, bg='#63CD41', relief='sunken')
+        addButton.place(relx=.5, rely=.7,anchor= CENTER)
         
         
 def main():
     root = Tk()
     app = Main(root)
     root.title("Main Page")
-    root.geometry("1350x750+350+200")
+    root.geometry("500x500+500+150")
     root.iconbitmap('IFLogo.ico')
     root.mainloop()
 
